6524a5247e282165b2f8c4dc44e3a7a75d92243e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-22 09:26:58 +0000

    Deploy lptai/personal-landing-page to github.com/lptai/personal-landing-page.git:gh-pages

---
+++ precache-manifest.0a1d811a7ebfe10b908110e074f4aed6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d3c7ac320b1ca93975014c8286974e0",
+    "url": "/personal-landing-page/index.html"
+  },
+  {
+    "revision": "b70769f7a291a23c3f5c",
+    "url": "/personal-landing-page/static/css/main.dedc6f4c.chunk.css"
+  },
+  {
+    "revision": "06997f21407c37ab7965",
+    "url": "/personal-landing-page/static/js/2.a55687c6.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/personal-landing-page/static/js/2.a55687c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b70769f7a291a23c3f5c",
+    "url": "/personal-landing-page/static/js/main.7280db6e.chunk.js"
+  },
+  {
+    "revision": "3c220247ba530acfdebf",
+    "url": "/personal-landing-page/static/js/runtime-main.00654296.js"
+  }
+]);
