57f51a5f1aa577006103ee312087dbd8427481cc
phuctaile <tai.le1@tyme.com>
2019-12-22 16:03:48 +0700

    update build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "http://phuctaile.com/personal-landing-page/public/precache-manifest.87926ce40c7c7b54ed6593101da34e11.js"
+  "/precache-manifest.d407843da70cfa1373c460573a11dc27.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://phuctaile.com/personal-landing-page/public/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
